4a0f28290dcc75ea9102c9b93fa40ec394310f91
Arissa2018 <arina.lachi@srco.utm.md>
2018-04-26 19:35:23 +0300

    performanta de azi

---
+++ src/tekwill_oca/Practice_04-1/src/practice_04_1/ShoppingCart.java
@@ -0,0 +1,14 @@
+/* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
+
+package practice_04_1;
+
+public class ShoppingCart {
+    public static void main(String[] args){
+        // Declare and initialize String variables.  Do not initialize message yet.
+      String custName="Mary Smith",itemDesc="wants to purchase a Shirt",message;
+        // Assign the message variable 
+        message=custName+" "+itemDesc+"!";
+        // Print and run the code
+System.out.println(message);
+    }
+}
