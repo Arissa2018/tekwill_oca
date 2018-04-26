ce32ec4b5022d4221e065686c2abd2802decd5d1
Arissa2018 <arina.lachi@srco.utm.md>
2018-04-26 19:54:13 +0300

    performanta de azi_1

--- src/tekwill_oca/Practice_04-1/src/practice_04_1/ShoppingCart.java
+++ src/tekwill_oca/Practice_04-1/src/practice_04_1/ShoppingCart.java
@@ -5,10 +5,16 @@ package practice_04_1;
 public class ShoppingCart {
     public static void main(String[] args){
         // Declare and initialize String variables.  Do not initialize message yet.
-      String custName="Mary Smith",itemDesc="wants to purchase a Shirt",message;
-        // Assign the message variable 
-        message=custName+" "+itemDesc+"!";
+      String custName="Mary Smith",itemDesc="wants to purchase=",message;
+      double price=20.53,tax=3.90,total;
+      int quantity=5;
+      total=quantity*20.53*3.90;
+
+        // Assign the message variable
+
+        message=custName+" "+itemDesc;
         // Print and run the code
-System.out.println(message);
+System.out.print(message);
+System.out.print(total);
     }
 }
